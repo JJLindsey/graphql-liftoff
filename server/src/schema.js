90381da94d1a75e9f4f95cd94a21eef43d78efde
JJLindsey <jlindsey010@gmail.com>
2021-10-08 16:51:29 -0700

    initial files

---
+++ server/src/schema.js
@@ -0,0 +1,23 @@
+const {gql} = require('apollo-server');
+
+const typeDefs = gql`
+
+"A track is a group of moduels that teaches about specific topic"
+type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+}
+
+"Author of a complete Track or a Module"
+type Author {
+    id: ID!
+    name: String!
+    photo: String
+}
+`;
+
+module.exports = typeDefs;
